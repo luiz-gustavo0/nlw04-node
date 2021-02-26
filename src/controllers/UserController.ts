a773185f11b2d537dc9d258093fcff84ced7cb96
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-02-26 14:27:50 -0300

    implementando classe AppErrors

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,23 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UserRepository";
+import * as yup from "yup";
+import { AppError } from "../errors/AppError";
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    try {
+      await schema.validate(request.body, { abortEarly: false });
+    } catch (err) {
+      throw new AppError(err);
+    }
+
     const usersRepository = getCustomRepository(UsersRepository);
 
     const userAlreadyExists = await usersRepository.findOne({
@@ -11,9 +25,7 @@ class UserController {
     });
 
     if (userAlreadyExists) {
-      return response.status(400).json({
-        error: "Useralready exists!",
-      });
+      throw new AppError("User already exists!");
     }
 
     const user = usersRepository.create({
